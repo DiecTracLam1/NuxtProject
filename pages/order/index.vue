64b2b4943f7c85834b697b68c8f00cc0235042f7
unknown <diectraclam47@gmail.com>
2023-06-16 17:03:48 +0700

    order

--- pages/order/index.vue
+++ pages/order/index.vue
@@ -4,16 +4,16 @@
   >
     <div class="">
       <a-tabs v-model:activeKey="activeKey" centered>
-        <a-tab-pane key="1" tab="Tab 1">
-            <template #tab>
-                <p class="mb-0">
-                    
-                </p>
-            </template>
+        <a-tab-pane key="1">
+          <template #tab>
+            <span class="text-base p-4"> All </span>
+          </template>
         </a-tab-pane>
-        <a-tab-pane key="2" tab="Tab 2" force-render
-          >Content of Tab Pane 2</a-tab-pane
-        >
+        <a-tab-pane key="2" force-render>
+          <template #tab>
+            <span class="text-base p-4"> Confirm </span>
+          </template>
+        </a-tab-pane>
         <a-tab-pane key="3" tab="Tab 3">Content of Tab Pane 3</a-tab-pane>
       </a-tabs>
     </div>
